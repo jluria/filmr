c74869ef434e4c9561e7633f93b134100ad7f01b
Wax <jwaxnyc@gmail.com>
2014-08-11 17:38:19 -0400

    create movies db

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811193529) do
+ActiveRecord::Schema.define(version: 20140811213338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: true do |t|
+    t.string "title"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                           null: false
