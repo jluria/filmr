bcf754e15e84ed80f5af33377f609fe4b145118f
Josh Luria <josh.luria@gmail.com>
2014-08-15 15:27:23 -0400

    Make movies timestamps more explicit

--- db/migrate/20140815190518_add_timestamps_to_movies.rb
+++ db/migrate/20140815190518_add_timestamps_to_movies.rb
@@ -1,6 +1,5 @@
 class AddTimestampsToMovies < ActiveRecord::Migration
   def change
-    add_column :movies, :created_at, :datetime
-    add_column :movies, :updated_at, :datetime
+    add_timestamps :movies
   end
 end
