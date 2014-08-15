30a2927fd605b55af1baa51032cac15b6e796619
Josh Luria <josh.luria@gmail.com>
2014-08-15 15:16:52 -0400

    Add timestamps to movies table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813160019) do
+ActiveRecord::Schema.define(version: 20140815190518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,9 @@ ActiveRecord::Schema.define(version: 20140813160019) do
   add_index "crew_memberships", ["talent_id"], name: "index_crew_memberships_on_talent_id", using: :btree
 
   create_table "movies", force: true do |t|
-    t.string "title"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "talents", force: true do |t|
